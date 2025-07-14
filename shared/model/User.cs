dfdadf806bb2c547b9361b94a3082d6dc2ab0c16
shlomyshivek <shlomyshivek@gmail.com>
2025-07-14 23:20:11 +0300

    comment out user model as it is not being used

--- shared/model/User.cs
+++ shared/model/User.cs
@@ -1,9 +1,9 @@
-namespace Tiba.Shared.Model;
+// namespace Tiba.Shared.Model;
 
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
+// public class User
+// {
+//     public int Id { get; set; }
+//     public string Name { get; set; } = string.Empty;
+//     public string Email { get; set; } = string.Empty;
+//     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+// }
